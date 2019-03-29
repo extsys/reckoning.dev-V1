2632d4c35b09c126a2e497486540b74333c5bfab
Sadanand Singh <sadanand4singh@gmail.com>
2019-03-29 15:23:05 -0700

    Initial commit from taniarascia repo

---
+++ src/pages/index.js
@@ -0,0 +1,175 @@
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../layout'
+import PostListing from '../components/PostListing'
+import ProjectListing from '../components/ProjectListing'
+import SimpleListing from '../components/SimpleListing'
+import NewsletterForm from '../components/NewsletterForm'
+import SEO from '../components/SEO'
+import config from '../../data/SiteConfig'
+import projects from '../../data/projects'
+import publications from '../../data/publications'
+
+class Index extends Component {
+  render() {
+    const latestPostEdges = this.props.data.latest.edges
+    const popularPostEdges = this.props.data.popular.edges
+    const published = publications.filter((article, i) => i < 6)
+
+    return (
+      <Layout>
+        <Helmet title={`${config.siteTitle} – Developer, designer, writer`} />
+        <SEO />
+        <div className="container">
+          <div className="lead">
+            <h1>
+              Hi, I'm Tania. I build <strong className="pink">open source projects</strong> and
+              write the <strong className="pink">missing instruction manuals</strong> of the web.
+            </h1>
+            <p>
+              I'm a <strong>developer, designer,</strong> and <strong>writer</strong>. I created
+              this site to document everything I learn, and share a bit of myself with the world. My
+              site is <strong>free</strong> and has no ads, affiliate links, or sponsored posts.
+            </p>
+            <a
+              className="twitter-follow-button"
+              href="https://twitter.com/taniarascia"
+              data-size="large"
+              data-show-screen-name="false"
+            >
+              Follow @sadannadsingh
+            </a>
+          </div>
+        </div>
+
+        <div className="container">
+          <section className="section">
+            <h2>Latest Articles</h2>
+            <PostListing simple postEdges={latestPostEdges} />
+          </section>
+
+          <section className="section">
+            <h2>Most Popular</h2>
+            <PostListing simple postEdges={popularPostEdges} />
+          </section>
+
+          <section className="section">
+            <h2>Open Source Projects</h2>
+            <ProjectListing projects={projects} />
+          </section>
+
+          <section className="section">
+            <h2>
+              Published Articles{' '}
+              <Link className="view-all" to="/publications">
+                View all
+              </Link>
+            </h2>
+            <SimpleListing simple data={published} />
+          </section>
+
+          <section className="section">
+            <h2>Other People's Opinions</h2>
+            <blockquote className="quotation">
+              <p>
+                You write extremely clear, concise tutorials that have the best ratio of learning to
+                bullshit that I've encountered so far. It's no exaggeration to say that I wouldn't
+                currently have a job in development without this site. So thanks for ruining my
+                life, Tania.
+              </p>
+              <cite>— Craig</cite>
+            </blockquote>
+            <blockquote className="quotation">
+              <p>
+                You taught me more than any class could have, and it took me a fraction of the time
+                because of how clearly you write and teach.
+              </p>
+              <cite>— Evan</cite>
+            </blockquote>
+            <blockquote className="quotation">
+              <p>
+                Not to get too dramatic, but I find your site to be an unspeakably beautiful
+                lifeboat in an overwhelming sea of technical jargon and shite.
+              </p>
+              <cite>— Lori</cite>
+            </blockquote>
+          </section>
+
+          <section className="section">
+            <h2>Newsletter</h2>
+            <p>Sign up to get notified about new content.</p>
+            <NewsletterForm />
+          </section>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Index
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    latest: allMarkdownRemark(
+      limit: 6
+      sort: { fields: [fields___date], order: DESC }
+      filter: { frontmatter: { template: { eq: "post" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+            date
+          }
+          excerpt
+          timeToRead
+          frontmatter {
+            title
+            tags
+            categories
+            thumbnail {
+              childImageSharp {
+                fixed(width: 150, height: 150) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+            date
+            template
+          }
+        }
+      }
+    }
+    popular: allMarkdownRemark(
+      limit: 6
+      sort: { fields: [fields___date], order: DESC }
+      filter: { frontmatter: { categories: { eq: "Popular" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+            date
+          }
+          excerpt
+          timeToRead
+          frontmatter {
+            title
+            tags
+            categories
+            thumbnail {
+              childImageSharp {
+                fixed(width: 150, height: 150) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+            date
+            template
+          }
+        }
+      }
+    }
+  }
+`
