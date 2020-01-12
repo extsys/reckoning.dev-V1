3e0f7763d0b70eb1a3aaeaea16d9f42857b19d0a
Sadanand Singh <sadanand4singh@gmail.com>
2020-01-11 19:38:23 -0800

    update footer icons to social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-import pytorch from '../../content/thumbnails/pytorch.png';
-import python from '../../content/thumbnails/python.png';
-import netlify from '../../content/thumbnails/netlify.png';
-import gatsby from '../../content/thumbnails/gatsby.png';
-import github from '../../content/thumbnails/github.png';
-import vscode from '../../content/thumbnails/vscode.png';
+import { IoLogoFacebook, IoLogoTwitter, IoLogoGithub, IoLogoLinkedin } from 'react-icons/io';
 
 export default class Footer extends Component {
   render() {
@@ -17,60 +12,26 @@ export default class Footer extends Component {
             &nbsp;&nbsp; reckoning.dev
           </a>
         </div>
-        <div>
-          <a href='https://pytorch.org' title='Pytorch Library'>
-            <img
-              src={pytorch}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='footer-img'
-              alt='Pytorch'
-            />
+        <div className='footer-link'>
+          <a
+            className='footer-link'
+            target='_blank'
+            href='https://www.facebook.com/sadanand4singh'
+          >
+            <IoLogoFacebook />
           </a>
-          <a href='https://www.python.org' title='Pytohon Programming'>
-            <img
-              src={python}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='footer-img'
-              alt='Python'
-            />
+          <a className='footer-link' target='_blank' href='https://github.com/sadanand-singh'>
+            <IoLogoGithub />
           </a>
-          <a href='https://code.visualstudio.com/' title='VSCode Editor'>
-            <img
-              src={vscode}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='footer-img'
-              alt='VSCode'
-            />
+          <a className='footer-link' target='_blank' href='https://twitter.com/reckoningdev'>
+            <IoLogoTwitter />
           </a>
-          <a href='https://github.com/sadanand-singh' title='Open-source on GitHub'>
-            <img
-              src={github}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='footer-img'
-              alt='GitHub'
-            />
-          </a>
-          <a href='https://www.netlify.com/' title='Hosted by Netlify'>
-            <img
-              src={netlify}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='footer-img'
-              alt='Netlify'
-            />
-          </a>
-          <a href='https://www.gatsbyjs.org/' title='Built with Gatsby'>
-            <img
-              src={gatsby}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='footer-img'
-              alt='Gatsby'
-            />
+          <a
+            className='footer-link'
+            target='_blank'
+            href='https://www.linkedin.com/in/sadanandsingh/'
+          >
+            <IoLogoLinkedin />
           </a>
         </div>
       </footer>
